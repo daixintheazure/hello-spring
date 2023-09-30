bc70cb92e93cc8cbc7e81c12dc5debd1bbc04341
Dai <dai.dickerson@gmail.com>
2023-09-30 02:54:26 -0500

    started hellospring new branch

--- src/main/java/org/launchcode/hellospring/controller/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controller/HelloController.java
@@ -1,14 +1,41 @@
 package org.launchcode.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("hello")
 public class HelloController {
+    //handle request at path http://localhost:8080/hello
 
-    @GetMapping
-    public String hello() {
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+
+    //handle request at path //http://localhost:8080/hello/goodbye
+    @GetMapping("goodbye")
+    public String goodbye(){
+        return "Goodbye, Spring!";
+    }
+
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    //handles requests for http://localhost:8080/hello/form
+    @GetMapping("form")
+    public String helloForm() {
         return "form";
     }
+
 }
